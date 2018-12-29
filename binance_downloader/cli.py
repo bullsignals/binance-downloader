0400a5cdb79f07566d96ddd34097d405f09753dd
Anson VanDoren <anson.vandoren@gmail.com>
2018-12-28 18:58:55 -0800

    Improved CLI help message

--- binance_downloader/cli.py
+++ binance_downloader/cli.py
@@ -1,54 +1,50 @@
-"""Command Line Interface"""
 import argparse
-
 from logbook import Logger
 
-from binance_downloader.api import BinanceAPI
-from binance_downloader.binance_utils import date_to_milliseconds
+from .api import BinanceAPI
+from .binance_utils import date_to_milliseconds
 
 log = Logger(__name__)
 
 
 def main():
-    """Initial Command Line Function."""
-
     parser = argparse.ArgumentParser(
-        description="Python tool to download Binance Candlestick (k-line) data from REST API"
+        description="CLI for downloading Binance Candlestick (k-line) data in bulk"
     )
     parser.add_argument(
-        "--interval",
-        "-i",
-        help="""
-                        frequency interval in minutes(m); hours(h); days(d); weeks(w); months(M);
-                        all possibles values: 
-                        1m 3m 5m 15m 30m 1h 2h 4h 6h 8h 12h 1d 3d 1w 1M""",
-        required=True,
+        "symbol",
+        help="(Required) Binance symbol pair, e.g. ETHBTC",
     )
     parser.add_argument(
-        "--symbol", "-s", help="pair. default: 'ETHBTC'.", default="ETHBTC"
+        "interval",
+        help="(Required) Frequency interval in minutes(m); hours(h); days(d); weeks(w); months(M);"
+        " All possibles values: 1m 3m 5m 15m 30m 1h 2h 4h 6h 8h 12h 1d 3d 1w 1M",
     )
     parser.add_argument(
-        "--start", "-st", help="Start period to get data. format: yyyy/mm/dd"
+        "--start", help="Start date to get data (inclusive). Format: yyyy/mm/dd"
     )
     parser.add_argument(
-        "--end", "-e", help="End period to get data (exclusive). format: yyyy/mm/dd"
+        "--end", help="End date to get data (exclusive). Format: yyyy/mm/dd"
     )
-    parser.add_argument("--output", "-o", help="File Name. default: binance")
+    parser.add_argument(
+        "--output",
+        help="File name to write data. Default: ./downloaded/timestamp_symbol_interval",
+    )
     # Allow to choose MM/DD/YYYY for date input
     parser.add_argument(
-        "--dateformat",
-        "-df",
-        help="Format to use for dates (DMY, MDY, YMD, etc). Defaults to `YMD`",
+        "--dtfmt",
+        metavar="DATE_FORMAT",
+        help="Format to use for dates (DMY, MDY, YMD, etc). Default: YMD",
         default="YMD",
     )
 
     args = parser.parse_args()
 
-    if args.dateformat:
-        if args.dateformat in ["DMY", "MDY", "YMD"]:
-            date_format = args.dateformat
+    if args.dtfmt:
+        if args.dtfmt in ["DMY", "MDY", "YMD"]:
+            date_format = args.dtfmt
         else:
-            log.warn(f"dateformat given ({args.dateformat}) not known. Using YMD")
+            log.warn(f"Date format given ({args.dtfmt}) not known. Using YMD")
             date_format = "YMD"
     else:
         date_format = "YMD"
